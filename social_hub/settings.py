bff00ba74a3ae8ea55248c327ba5d22a9c7e6875
Alo Victor <vicsnikky@gmail.com>
2025-05-20 11:12:23 +0100

    Use environment variable for SECRET_KEY

--- social_hub/settings.py
+++ social_hub/settings.py
@@ -5,7 +5,11 @@ import dj_database_url
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'secret-key-here'
+SECRET_KEY = 'soyz@lyzxsvw#w-&x7@=))nv0vlt0(qjad3y4o040fclchwyu$'
+
+import os
+SECRET_KEY = os.environ.get("soyz@lyzxsvw#w-&x7@=))nv0vlt0(qjad3y4o040fclchwyu$")
+
 
 
 DEBUG = True
